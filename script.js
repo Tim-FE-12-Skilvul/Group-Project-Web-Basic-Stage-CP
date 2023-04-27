db0bc186427814b151727bf3183a9899323b4149
Dedy Lumbantobing <dedylumbantobing3@gmail.com>
2023-04-27 13:13:09 +0700

    Menambahkan kode javascript untuk menjalankan responsive pada navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('#hamburger-container');
+const menuNav = document.querySelector('#menuNav');
+const pencarianNav = document.querySelector('#pencarianNav');
+
+hamburger.addEventListener('click', () => {
+  menuNav.classList.toggle('active');
+  pencarianNav.classList.toggle('active');
+});
