94b7ce4839f9b3b96e9d2cc8b1082de69ca5a21c
rhyo-arga <123200059@student.upnyk.ac.id>
2023-05-06 23:21:19 +0700

    Menambahkan Tampilan Pada Menu Berita dan Detail Berita

--- newsapi.js
+++ newsapi.js
@@ -6,6 +6,23 @@ async function logJSONData() {
     const response = await fetch(endpoint);
     const jsonData = await response.json();
     console.log(jsonData);
+    for (i = 0; i < jsonData.length; i++) {
+        // console.log(jsonData[i]["title"]);
+        // display data to html
+        let articles =
+            `
+            <div class="col-md-4">
+                <div class = "card" style = "width: 18rem;" >
+                    <img src = "${jsonData[i]["urlToImage"]}" class = "card-img-top" alt = "" >
+                    <div class = "card-body" >
+                        <h5 class = "card-title">${jsonData[i]["title"]}</h5> <p class = "card-text">${jsonData[i]["description"]}</p> 
+                        <a href = "detail_berita.html?id=${jsonData[i]["id"]}" class = "card-link"> Baca Selengkapnya </a> 
+                    </div > 
+                </div>
+            </div>
+            `
+        document.getElementById("listArticles").innerHTML += articles;
+    }
 }
 
 logJSONData();
